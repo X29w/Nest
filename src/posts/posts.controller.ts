9a35b0bc4103263a250f56b552c3fef9b0791082
園洛樱介 <1937009360@qq.com>
2023-11-05 11:38:49 +0800

    链接数据库

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
+
+class createPostsDTO {
+  @ApiProperty()
+  title: string;
+  @ApiProperty()
+  content: string;
+  @ApiProperty()
+  other?: string;
+}
+
+@Controller('posts')
+@ApiTags('文章')
+export class PostsController {
+  @Get()
+  @ApiOperation({ summary: '列表' })
+  index() {
+    return [];
+  }
+
+  @Post()
+  @ApiOperation({ summary: '创建' })
+  create(@Body() body: createPostsDTO) {
+    return {
+      data: body,
+      success: true,
+    };
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: '查看详情' })
+  detail(@Param('id') id: number) {
+    return {
+      id,
+      title: 'x29',
+    };
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: '编辑' })
+  update(@Param('id') id: number, @Body() bdy: createPostsDTO) {
+    return {
+      success: true,
+    };
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '删除' })
+  remove(@Param('id') id: number) {
+    return {
+      success: true,
+    };
+  }
+}
